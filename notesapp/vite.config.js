fa7a8084204aacf496d98bd6f02918fc628b543f
dataskeptic <mateus_henrique2011@outlook.com>
2024-11-29 15:04:04 -0300

    creating front end for auth

--- notesapp/vite.config.js
+++ notesapp/vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()], // Ensures React-specific features work properly
   build: {
-    outDir: 'dist', 
+    outDir: 'dist', // Output directory
   },
-})
+});
